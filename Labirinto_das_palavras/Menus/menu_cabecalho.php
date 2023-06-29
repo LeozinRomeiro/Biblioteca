59214526c3540df01fcbea890fff1022e9ca050e
Leonardo Romeiro <123381157+LeozinRomeiro@users.noreply.github.com>
2023-06-29 00:21:13 -0300

    Add files via upload

---
+++ Labirinto_das_palavras/Menus/menu_cabecalho.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Labirinto das Palavras</title>
+    <link rel="stylesheet" type="text/css" href="../Labirinto_das_palavras/CSS/style.css">
+</head>
+<body>
+    <br><br>    
+    <a class="a_cabecalho" href="menu_locar.php">Locar</a>  
+    <a class="a_cabecalho" href="menu_alunos.php">Alunos</a>    
+    <a class="a_cabecalho" href="menu_livros.php">Livros</a>
+</body>
+</html>
---
+++ Labirinto_das_palavras/Menus/menu_locar.php
@@ -0,0 +1,17 @@
+<?php include '/xampp/htdocs/Labirinto_das_palavras/Menus/menu_cabecalho.php'; //Incluir o menu inicial na parte de cima ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Locar Livros</title>
+    <link rel="stylesheet" type="text/css" href="../CSS/style.css">
+</head>
+<body>
+    <h1>Locar</h1>
+
+    <div class="div_menu">
+        <li class="li_menu"><a class="a_menu" href="../Locar/html_locar.php">Registrar locação</a><br></li>
+    </div>
+
+</body>
+</html>
